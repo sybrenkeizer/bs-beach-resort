95ede79167b0c7ccee561e6764c143d4fbe06bd5
Sybren <92291687+sybrenkeizer@users.noreply.github.com>
2022-08-19 00:28:50 +0545

    Initial commit

---
+++ custom-script.js
@@ -0,0 +1,18 @@
+const navEl = document.getElementById("navigation");
+const navToggleEL = document.querySelector(".navbar-toggler");
+
+// Change navbar background on scroll
+window.addEventListener("scroll", (e) =>
+  window.pageYOffset > 20 
+    ? navEl.classList.add("nav-dark")
+    : window.pageYOffset < 20 && navToggleEL.ariaExpanded === "true"
+    ? navEl.classList.add("nav-dark")
+    : navEl.classList.remove("nav-dark")
+);
+
+// Change navbar background when expanded
+navToggleEL.addEventListener("click", (e) => 
+  navToggleEL.ariaExpanded === "true"
+    ? navEl.classList.add("nav-dark")
+    : navEl.classList.remove("nav-dark")
+);
