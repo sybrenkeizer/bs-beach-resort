cc0e108a5b5df53861e1894fed448c5632b95d00
Sybren <92291687+sybrenkeizer@users.noreply.github.com>
2022-08-21 11:39:22 +0545

    Improve UI

--- custom-script.js
+++ custom-script.js
@@ -2,17 +2,17 @@ const navEl = document.getElementById("navigation");
 const navToggleEL = document.querySelector(".navbar-toggler");
 
 // Change navbar background on scroll
-window.addEventListener("scroll", (e) =>
-  window.pageYOffset > 20 
-    ? navEl.classList.add("nav-dark")
-    : window.pageYOffset < 20 && navToggleEL.ariaExpanded === "true"
-    ? navEl.classList.add("nav-dark")
-    : navEl.classList.remove("nav-dark")
-);
+// window.addEventListener("scroll", (e) =>
+//   window.pageYOffset > 20 
+//     ? navEl.classList.add("nav-dark")
+//     : window.pageYOffset < 20 && navToggleEL.ariaExpanded === "true"
+//     ? navEl.classList.add("nav-dark")
+//     : navEl.classList.remove("nav-dark")
+// );
 
 // Change navbar background when expanded
-navToggleEL.addEventListener("click", (e) => 
-  navToggleEL.ariaExpanded === "true"
-    ? navEl.classList.add("nav-dark")
-    : navEl.classList.remove("nav-dark")
-);
+// navToggleEL.addEventListener("click", (e) => 
+//   navToggleEL.ariaExpanded === "true"
+//     ? navEl.classList.add("nav-dark")
+//     : navEl.classList.remove("nav-dark")
+// );
